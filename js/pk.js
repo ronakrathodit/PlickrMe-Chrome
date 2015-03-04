3723e7ed1dfb706192d7833f002f5c578ffc44fd
Rafique Mohammed <dev.rafiq@live.com>
2015-03-04 20:13:46 +0530

    added card layout

---
+++ js/pk.js
@@ -0,0 +1,4 @@
+/**
+ * Created by Ronak on 3/4/2015.
+ */
+var pkgrabdelete="http://192.168.0.200/plick_me/plickr_api/User/Grab/";
--- js/rightclick.js
+++ js/rightclick.js
@@ -6,6 +6,57 @@ var username = "";
 var email, AuthID, device_id;
 var contextID = null;
 var isLogged = false;
+var senderIds = ["362496581007"];
+
+var uuid = function() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random()*16|0,v=c=='x'?r:r&0x3|0x8;return v.toString(16);
+    });
+};
+
+console.log("uuid "+uuid());
+
+function registerCallback(registrationId) {
+    if (chrome.runtime.lastError) {
+        // When the registration fails, handle the error and retry the
+        // registration later.
+        console.log("registerCallback Error");
+        return;
+    }
+    console.log("registerCallback 1 id "+registrationId);
+    // Send the registration ID to your application server.
+    sendRegistrationId(function(succeed) {
+        console.log("registerCallback 2 id "+succeed);
+        // Once the registration ID is received by your server,
+        // set the flag such that register will not be invoked
+        // next time when the app starts up.
+        if (succeed)
+            chrome.storage.local.set({gcm:{gcm_id:registrationId,registered: true}});
+    });
+}
+
+function sendRegistrationId(callback) {
+    console.log("sendRegistrationId  func "+callback);
+
+    callback(true);
+    // Send the registration ID to your application server
+    // in a secure way.
+}
+
+chrome.runtime.onStartup.addListener(function() {
+    chrome.storage.local.get("gcm", function(result) {
+        // If already registered, bail out.
+        console.log(result);
+        if (result.gcm.registered)
+
+            return;
+
+        // Up to 100 senders are allowed.
+
+        chrome.gcm.register(senderIds, registerCallback);
+    });
+});
+
 var dataSentCallback=function(pay,isSent){
     if(isSent){
         pay.synced=1;
